1e6b83a8ccbb5cfd8bf761cb3e3f5dd87fc3a637
abdulsalamemesh <abdulsalam.emesh.work@gmail.com>
2021-09-30 10:34:57 +0200

    test the datatable class

--- tests/RenderTest.php
+++ tests/RenderTest.php
@@ -3,7 +3,6 @@
 namespace LasePeCo\Datatables\Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\View;
 use LasePeCo\Datatables\Http\Livewire\Datatable;
 use LasePeCo\Datatables\Tests\Factories\FakeModelFactory;
 use LasePeCo\Datatables\Tests\Models\FakeModel;
@@ -14,14 +13,83 @@ class RenderTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function it_shows_columns(){
-        //<livewire:datatable model="App\Models\User" />
-
+    public function it_shows_columns()
+    {
+        //<livewire:datatable model="App\Models\User"/>
         factory(FakeModel::class, 5)->create();
 
-        Livewire::test(Datatable::class ,  ['model' => FakeModel::class])->assertSeeHtmlInOrder([
-            '<td>Name</td>',
-            '<td>Email</td>',
+        Livewire::test(Datatable::class, ['model' => FakeModel::class])->assertSeeHtmlInOrder([
+            'name',
+            'email',
         ]);
+    }
+
+    /** @test */
+    public function it_shows_only_included_columns()
+    {
+        //<livewire:datatable model="App\Models\User" include="name, email"/>
+        //<livewire:datatable model="App\Models\User" include="name,email"/>
+        factory(FakeModel::class, 5)->create();
+
+        Livewire::test(Datatable::class, ['model' => FakeModel::class, 'include' => 'name,email'])->assertSeeHtmlInOrder([
+            'name',
+            'email',
+        ])->assertDontSeeText(['id', 'Id', 'ID']);
+
+        Livewire::test(Datatable::class, ['model' => FakeModel::class, 'include' => 'name, email'])->assertSeeHtmlInOrder([
+            'name',
+            'email',
+        ])->assertDontSeeText(['id', 'Id', 'ID']);
+    }
+
+    /** @test */
+    public function it_shows_all_columns_except_the_excluded()
+    {
+        //<livewire:datatable model="App\Models\User" exclude="name,id"/>
+        factory(FakeModel::class, 5)->create();
+
+        Livewire::test(Datatable::class, ['model' => FakeModel::class, 'exclude' => 'name'])->assertSeeHtmlInOrder([
+            'email',
+        ])->assertDontSeeText('name');
+    }
+
+    /** @test */
+    public function it_shows_data()
+    {
+        $fakeModels = factory(FakeModel::class, 5)->create();
+
+        Livewire::test(Datatable::class, ['model' => FakeModel::class])->assertSeeText($fakeModels->first()->email);
+    }
+
+    /** @test */
+    public function it_paginates_data()
+    {
+        $fakeModels = factory(FakeModel::class, 50)->create();
+
+        Livewire::test(Datatable::class, ['model' => FakeModel::class])
+            ->set('pagination', 25)
+            ->assertSet('pagination', 25)
+            ->assertDontSeeText($fakeModels->first()->name);
+
+        Livewire::test(Datatable::class, ['model' => FakeModel::class])
+            ->set('pagination', 'All')
+            ->assertSet('pagination', 'All')
+            ->assertDontSeeText($fakeModels->first()->name);
+    }
+
+    /** @test */
+    public function it_sorts_data()
+    {
+        factory(FakeModel::class, 10)->create();
+
+        Livewire::test(Datatable::class, ['model' => FakeModel::class])
+            ->call('sort', 'name')
+            ->assertSet('sort_by', 'name')
+            ->assertSet('sort_direction', 'asc');
+
+        Livewire::test(Datatable::class, ['model' => FakeModel::class])
+            ->call('sort', 'id')
+            ->assertSet('sort_by', 'id')
+            ->assertSet('sort_direction', 'asc');
     }
 }
