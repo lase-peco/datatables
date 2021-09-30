eb24fa24e37c295572ab55b064d838fdb9f5a1e7
abdulsalamemesh <abdulsalam.emesh.work@gmail.com>
2021-09-30 10:35:56 +0200

    implement the basic datatable

--- resources/views/livewire/datatable.blade.php
+++ resources/views/livewire/datatable.blade.php
@@ -1,5 +1,63 @@
-<table>
-    @foreach($columns as $column)
-        <td>{{ $column }}</td>
-    @endforeach
-</table>
+<div class="flex flex-col">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                    <tr>
+                        @foreach($columns as $column)
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider cursor-pointer align-middle whitespace-nowrap"
+                                wire:click="sort('{{$column}}')">
+                                @if($this->sort_by == $column)
+                                    @if($this->sort_direction == 'desc')
+                                        {{-- https://fontawesome.com/v5.15/icons/sort-up?style=solid--}}
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="inline-block w-2 fill-current text-gray-500">
+                                            <path d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41z"/>
+                                        </svg>
+                                    @else
+                                        {{-- https://fontawesome.com/v5.15/icons/sort-down?style=solid--}}
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="inline-block w-2 fill-current text-gray-500">
+                                            <path d="M279 224H41c-21.4 0-32.1-25.9-17-41L143 64c9.4-9.4 24.6-9.4 33.9 0l119 119c15.2 15.1 4.5 41-16.9 41z"/>
+                                        </svg>
+                                    @endif
+                                @else
+                                    {{--https://fontawesome.com/v5.15/icons/sort?style=solid--}}
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="inline-block w-2 fill-current text-gray-500">
+                                        <path
+                                            d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41zm255-105L177 64c-9.4-9.4-24.6-9.4-33.9 0L24 183c-15.1 15.1-4.4 41 17 41h238c21.4 0 32.1-25.9 17-41z"/>
+                                    </svg>
+                                @endif
+                                {{ str_replace('_', ' ', $column) }}
+                            </th>
+                        @endforeach
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($data as $row)
+                        <tr class="odd:bg-white even:bg-gray-50">
+                            @foreach($columns as $key => $column)
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    {{ $row->{$key} }}
+                                </td>
+                            @endforeach
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                <div class="py-2 px-4 flex content-center">
+                    <div class="mr-2">
+                        <select  wire:model="pagination" id="pagination" name="pagination" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+                            @foreach([10,25,50,100,'All'] as $count)
+                                <option value="{{$count}}">{{$count}}</option>
+                            @endforeach
+
+                        </select>
+                    </div>
+                    <div class="w-full">
+                        {{ $data->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- src/Http/Livewire/Datatable.php
+++ src/Http/Livewire/Datatable.php
@@ -2,21 +2,48 @@
 
 namespace LasePeCo\Datatables\Http\Livewire;
 
-use Illuminate\Foundation\Auth\User;
-use Illuminate\Support\Arr;
-use LasePeCo\Datatables\Tests\Models\FakeModel;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Datatable extends Component
 {
+    use WithPagination;
+
     /**
      * @var \Illuminate\Database\Eloquent\Model
      */
-    private $model;
+    public $model;
 
-    public function mount($model)
+    /**
+     * @var string
+     */
+    public $include;
+
+    /**
+     * @var string
+     */
+    public $exclude;
+
+    /**
+     * @var string
+     */
+    public $sort_by;
+
+    /**
+     * @var string
+     */
+    public $sort_direction;
+
+
+    public $pagination;
+
+    public function mount($model, $include = null, $exclude = null, $pagination = 10)
     {
         $this->model = $model;
+        $this->include = collect(explode(',', $include))->map(fn($key) => trim($key))->filter()->unique()->toArray();
+        $this->exclude = collect(explode(',', $exclude))->map(fn($key) => trim($key))->filter()->unique()->toArray();
+        $this->pagination = $pagination;
+
     }
 
     /**
@@ -24,18 +51,70 @@ class Datatable extends Component
      */
     private function query()
     {
+
         return new $this->model;
     }
 
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function render()
     {
-        return view('datatables::livewire.datatable')->with([
-            'columns' => $this->getColumns(),
-        ]);
+        $columns = $this->getColumns();
+
+        $data = $this->query();
+
+        if (is_null($this->sort_by)) {
+            $this->sort_by = array_key_first($columns);
+            $this->sort_direction = 'desc';
+        }
+
+        $data = $data->orderBy($this->sort_by, $this->sort_direction);
+
+        if ($this->pagination == "All"){
+            $data=  $data->paginate();
+        }else{
+            $data=  $data->paginate($this->pagination);
+        }
+
+        return view('datatables::livewire.datatable', compact('columns', 'data'));
     }
 
-    private function getColumns()
+    /**
+     * @return array
+     */
+    private function getColumns(): array
     {
-        return collect($this->query()->first()->getAttributes())->keys()->map(fn($key) => ucfirst($key))->toArray();
+        $columns = collect($this->query()->first()->getAttributes());
+
+        if (count($this->exclude) > 0) {
+            $columns = $columns->except($this->exclude);
+        }
+
+        if (count($this->include) > 0) {
+            $sortedColumns = collect();
+
+            foreach ($this->include as $key) {
+                $sortedColumns[$key] = $columns[$key];
+            }
+
+            $columns = $sortedColumns;
+        }
+
+        return $columns->map(fn($value, $key) => $key)->toArray();
+    }
+
+    /**
+     * @param $attribute
+     */
+    public function sort($attribute)
+    {
+        if ($this->sort_by != $attribute) {
+            $this->sort_direction = 'asc';
+        } else {
+            $this->sort_direction = $this->sort_direction == 'asc' ? 'desc' : 'asc';
+        }
+
+        $this->sort_by = $attribute;
     }
 }
